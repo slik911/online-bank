7034a0113b2a55569c50eb2ecff0c84a0b54f8fa
Tony Starks <35855530+slik911@users.noreply.github.com>
2023-07-25 00:36:24 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,8 @@
         <meta charset="utf-8" />
         <title>Turkiye Sedaf Finans</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-        <meta content="Themesbrand" name="author" />
+        <meta content="Online Banking, Deposits, Transfers, Savings and Loans" name="description" />
+        <meta content="TurkiyeSedafFinans" name="author" />
         <!-- App favicon -->
         <link rel="shortcut icon" href="assets/images/favicon.ico">
 
